b95191c5f141b9a13f2a95515a7d550873a62166
Hugo-Lourenco <hugo.lourenco@ynov.com>
2026-05-18 11:36:40 +0200

    game

--- main.c
+++ main.c
@@ -14,8 +14,10 @@ bool running = true;
 Player p1;
 bool window_focused = true;
 
+// Tableau pour stocker les infos de tout le monde
 Player Packets_players[5]; 
 
+// Fonction de détection des tirs (Hitscan)
 bool check_if_hit(PlayerPacket shooter, Player target) {
     float dx = target.x - shooter.x;
     float dy = target.y - shooter.y;
@@ -65,6 +67,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     
+    // --- CONSOLE TEMPORAIRE POUR LE MENU ---
     AllocConsole();
     freopen("CONIN$", "r", stdin);
     freopen("CONOUT$", "w", stdout);
@@ -79,15 +82,16 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     printf("Choix : ");
     scanf("%d", &choix);
 
-    printf("Entrez votre ID joueur (1, 2, 3 ou 4) : ");
+    printf("Entrez votre ID unique (1, 2, 3 ou 4) : ");
     scanf("%d", &mon_id);
 
     if (choix == 2) {
         printf("Entrez l'IP de l'hebergeur : ");
         scanf("%s", ip_serveur);
     }
-    FreeConsole();
+    FreeConsole(); // On détruit la console, place au jeu !
 
+    // --- INITIALISATIONS ---
     init_player(&p1, mon_id, 2.0f, 2.0f);
     if (!init_networking()) return 0;
     int mon_socket = setup_udp_socket(choix == 1);
@@ -103,38 +107,42 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     HWND hwnd = CreateWindowEx(0, CLASS_NAME, "CS:GO Retro 3D (GDI)", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, SCREEN_WIDTH, SCREEN_HEIGHT, NULL, NULL, hInstance, NULL);
     if (hwnd == NULL) { closesocket(mon_socket); clean_networking(); return 0; }
+    
     ShowWindow(hwnd, nCmdShow);
+    SetForegroundWindow(hwnd); // FORCE LE JEU AU PREMIER PLAN !
 
     MSG msg = {0};
     static DWORD last_shot_time = 0; 
 
-    // --- VARIABLES POUR SAUVEGARDER LE CLIENT ---
-    struct sockaddr_in client_addr;
-    bool client_connected = false;
+    // --- LE REGISTRE DES CLIENTS DU SERVEUR ---
+    struct sockaddr_in clients_addr[5];
+    bool is_client_connected[5] = {false, false, false, false, false};
 
+    // --- BOUCLE PRINCIPALE ---
     while (running) {
         while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
 
+        // --- RESPAWN ---
         if (!p1.is_alive) {
-            if (GetAsyncKeyState('R') & 0x8000) {
+            if (window_focused && (GetAsyncKeyState('R') & 0x8000)) {
                 init_player(&p1, p1.id, 2.0f, 2.0f);
             }
         }
 
         bool shooting_now = false;
-        if (p1.is_alive) {
-            if (window_focused) {
-                POINT center_pt = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-                ClientToScreen(hwnd, &center_pt); 
-                POINT mouse_pos;
-                GetCursorPos(&mouse_pos);
-                int delta_x = mouse_pos.x - center_pt.x;
-                p1.angle += (float)delta_x * 0.0025f;
-                SetCursorPos(center_pt.x, center_pt.y);
-            }
+        
+        // --- CONTRÔLES ---
+        if (p1.is_alive && window_focused) {
+            POINT center_pt = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+            ClientToScreen(hwnd, &center_pt); 
+            POINT mouse_pos;
+            GetCursorPos(&mouse_pos);
+            int delta_x = mouse_pos.x - center_pt.x;
+            p1.angle += (float)delta_x * 0.0025f;
+            SetCursorPos(center_pt.x, center_pt.y);
 
             float speed = 0.05f;
             if (GetAsyncKeyState('Z') & 0x8000) {
@@ -162,17 +170,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
             }
         }
 
-        if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) running = false;
+        if (window_focused && (GetAsyncKeyState(VK_ESCAPE) & 0x8000)) running = false;
 
-        // --- RÉCEPTION ET DÉTECTION DE HIT ---
+        // --- RÉCEPTION ET DÉTECTION ---
         PlayerPacket paquet_recu;
         struct sockaddr_in adresse_provenance;
         while (receive_data(mon_socket, &paquet_recu, &adresse_provenance)) {
             
-            // LA MAGIE EST ICI : Le serveur mémorise qui vient de lui parler !
-            if (choix == 1) {
-                client_addr = adresse_provenance;
-                client_connected = true;
+            // Le Serveur enregistre chaque joueur !
+            if (choix == 1 && paquet_recu.id >= 1 && paquet_recu.id <= 4) {
+                clients_addr[paquet_recu.id] = adresse_provenance;
+                is_client_connected[paquet_recu.id] = true;
             }
 
             if (paquet_recu.id != p1.id && paquet_recu.id >= 1 && paquet_recu.id <= 4) {
@@ -181,27 +189,40 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
                 Packets_players[paquet_recu.id].angle = paquet_recu.angle;
                 Packets_players[paquet_recu.id].hp = paquet_recu.hp;
                 Packets_players[paquet_recu.id].is_alive = (paquet_recu.hp > 0);
+                Packets_players[paquet_recu.id].is_shooting = paquet_recu.is_shooting; 
 
                 if (paquet_recu.is_shooting && p1.is_alive) {
                     if (check_if_hit(paquet_recu, p1)) {
                         p1.hp -= 25; 
-                        if (p1.hp <= 0) {
-                            p1.hp = 0;
-                            p1.is_alive = false;
-                        }
+                        if (p1.hp <= 0) { p1.hp = 0; p1.is_alive = false; }
                     }
                 }
             }
         }
 
-        // --- ENVOI DES INFOS ---
+        // --- ENVOI ET RELAIS DES INFOS ---
         PlayerPacket mon_paquet = { p1.id, p1.x, p1.y, p1.angle, p1.hp, shooting_now };
+        
         if (choix == 2) {
-            // Le Client envoie au Serveur
             send_data(mon_socket, mon_paquet, ip_serveur);
-        } else if (choix == 1 && client_connected) {
-            // Le Serveur renvoie ses infos à l'adresse du Client qu'il a mémorisée !
-            sendto(mon_socket, (char*)&mon_paquet, sizeof(PlayerPacket), 0, (struct sockaddr*)&client_addr, sizeof(client_addr));
+        } 
+        else if (choix == 1) {
+            // Le Serveur relaie !
+            for (int i = 2; i <= 4; i++) {
+                if (is_client_connected[i]) {
+                    sendto(mon_socket, (char*)&mon_paquet, sizeof(PlayerPacket), 0, (struct sockaddr*)&clients_addr[i], sizeof(clients_addr[i]));
+                    
+                    for (int j = 2; j <= 4; j++) {
+                        if (i != j && is_client_connected[j]) {
+                            PlayerPacket relay_pkt = { 
+                                j, Packets_players[j].x, Packets_players[j].y, 
+                                Packets_players[j].angle, Packets_players[j].hp, Packets_players[j].is_shooting 
+                            };
+                            sendto(mon_socket, (char*)&relay_pkt, sizeof(PlayerPacket), 0, (struct sockaddr*)&clients_addr[i], sizeof(clients_addr[i]));
+                        }
+                    }
+                }
+            }
         }
 
         // --- GRAPHISMES ---
--- player.h
+++ player.h
@@ -1,15 +1,19 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+
 #include <stdbool.h>
 
 typedef struct {
     int id;
-    float x, y;     // Position sur la grille (ex: x=1.5, y=2.3)
-    float angle;    // Direction du regard en radians
+    float x;
+    float y;
+    float angle;
     int hp;
     bool is_alive;
+    bool is_shooting; // <-- C'est l'information qui manquait !
 } Player;
 
+// Initialise un joueur avec son ID et sa position de départ
 void init_player(Player* p, int id, float start_x, float start_y);
 
 #endif

d46e82e0835440f4532f2d3cacab627b7384a2dd
Hugo-Lourenco <hugo.lourenco@ynov.com>
2026-06-02 10:25:13 +0200

    noir

--- network.c
+++ network.c
@@ -1,15 +1,12 @@
 #include "network.h"
 #include <stdio.h>
 #include <string.h>
-#include <winsock2.h>
 #include <iphlpapi.h>
 
 // Initialise la bibliothèque réseau de Windows (Winsock)
-bool init_networking()
-{
+bool init_networking() {
     WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-    {
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
         printf("Erreur d'initialisation Winsock. Code : %d\n", WSAGetLastError());
         return false;
     }
@@ -17,11 +14,9 @@ bool init_networking()
 }
 
 // Crée et configure le socket UDP
-int setup_udp_socket(bool is_server)
-{
+int setup_udp_socket(bool is_server) {
     int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (sock == INVALID_SOCKET)
-    {
+    if (sock == INVALID_SOCKET) {
         printf("Impossible de créer le socket UDP.\n");
         return -1;
     }
@@ -29,18 +24,16 @@ int setup_udp_socket(bool is_server)
     unsigned long mode = 1;
     ioctlsocket(sock, FIONBIO, &mode);
 
-    if (is_server)
-    {
+    if (is_server) {
         int reuse = 1;
-        setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse));
+        setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char*)&reuse, sizeof(reuse));
 
         struct sockaddr_in server;
         server.sin_family = AF_INET;
         server.sin_addr.s_addr = INADDR_ANY;
         server.sin_port = htons(PORT);
 
-        if (bind(sock, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
-        {
+        if (bind(sock, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
             printf("Erreur de liaison (bind). Code : %d\n", WSAGetLastError());
             return -1;
         }
@@ -49,16 +42,14 @@ int setup_udp_socket(bool is_server)
 }
 
 // Active le broadcast sur le socket
-void enable_broadcast(int sock)
-{
+void enable_broadcast(int sock) {
     int opt = 1;
-    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char *)&opt, sizeof(opt));
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char*)&opt, sizeof(opt));
 }
 
 // Calcule les adresses de broadcast de toutes les interfaces réseau actives
 // Retourne le nombre d'adresses trouvées
-static int get_broadcast_addresses(unsigned long *bcast_addrs, int max_addrs)
-{
+static int get_broadcast_addresses(unsigned long* bcast_addrs, int max_addrs) {
     int count = 0;
 
     // Toujours ajouter 255.255.255.255 en fallback
@@ -68,37 +59,28 @@ static int get_broadcast_addresses(unsigned long *bcast_addrs, int max_addrs)
     IP_ADAPTER_INFO adapterInfo[16];
     DWORD bufLen = sizeof(adapterInfo);
 
-    if (GetAdaptersInfo(adapterInfo, &bufLen) == NO_ERROR)
-    {
-        IP_ADAPTER_INFO *adapter = adapterInfo;
-        while (adapter && count < max_addrs)
-        {
+    if (GetAdaptersInfo(adapterInfo, &bufLen) == NO_ERROR) {
+        IP_ADAPTER_INFO* adapter = adapterInfo;
+        while (adapter && count < max_addrs) {
             // Récupérer IP et masque
-            unsigned long ip = inet_addr(adapter->IpAddressList.IpAddress.String);
+            unsigned long ip   = inet_addr(adapter->IpAddressList.IpAddress.String);
             unsigned long mask = inet_addr(adapter->IpAddressList.IpMask.String);
 
             // Ignorer les interfaces invalides ou loopback
-            if (ip != 0 && ip != inet_addr("127.0.0.1") && mask != 0)
-            {
+            if (ip != 0 && ip != inet_addr("127.0.0.1") && mask != 0) {
                 // Broadcast = (IP & masque) | (~masque)
                 unsigned long bcast = (ip & mask) | (~mask);
 
                 // Éviter les doublons
                 bool already = false;
-                for (int i = 0; i < count; i++)
-                {
-                    if (bcast_addrs[i] == bcast)
-                    {
-                        already = true;
-                        break;
-                    }
+                for (int i = 0; i < count; i++) {
+                    if (bcast_addrs[i] == bcast) { already = true; break; }
                 }
-                if (!already)
-                {
+                if (!already) {
                     printf("  Interface detectee : %s / %s -> broadcast %s\n",
                            adapter->IpAddressList.IpAddress.String,
                            adapter->IpAddressList.IpMask.String,
-                           inet_ntoa(*(struct in_addr *)&bcast));
+                           inet_ntoa(*(struct in_addr*)&bcast));
                     bcast_addrs[count++] = bcast;
                 }
             }
@@ -109,8 +91,7 @@ static int get_broadcast_addresses(unsigned long *bcast_addrs, int max_addrs)
 }
 
 // Client : envoie un broadcast sur toutes les interfaces et collecte les réponses
-int discover_servers(int sock, char found_ips[][64], int max_servers)
-{
+int discover_servers(int sock, char found_ips[][64], int max_servers) {
     enable_broadcast(sock);
 
     DiscoveryPacket req;
@@ -122,41 +103,35 @@ int discover_servers(int sock, char found_ips[][64], int max_servers)
     unsigned long bcast_addrs[8];
     int nb_ifaces = get_broadcast_addresses(bcast_addrs, 8);
 
-    for (int b = 0; b < nb_ifaces; b++)
-    {
+    for (int b = 0; b < nb_ifaces; b++) {
         struct sockaddr_in bcast;
         bcast.sin_family = AF_INET;
         bcast.sin_port = htons(PORT);
         bcast.sin_addr.s_addr = bcast_addrs[b];
-        sendto(sock, (char *)&req, sizeof(DiscoveryPacket), 0,
-               (struct sockaddr *)&bcast, sizeof(bcast));
+        sendto(sock, (char*)&req, sizeof(DiscoveryPacket), 0,
+               (struct sockaddr*)&bcast, sizeof(bcast));
     }
 
     int count = 0;
     DWORD start = GetTickCount();
 
-    while (GetTickCount() - start < 2000 && count < max_servers)
-    {
+    while (GetTickCount() - start < 2000 && count < max_servers) {
         DiscoveryPacket resp;
         struct sockaddr_in from;
         int from_len = sizeof(from);
 
-        int res = recvfrom(sock, (char *)&resp, sizeof(DiscoveryPacket), 0,
-                           (struct sockaddr *)&from, &from_len);
+        int res = recvfrom(sock, (char*)&resp, sizeof(DiscoveryPacket), 0,
+                           (struct sockaddr*)&from, &from_len);
 
-        if (res > 0 && resp.packet_type == PACKET_TYPE_DISCOVERY_RESP)
-        {
+        if (res > 0 && resp.packet_type == PACKET_TYPE_DISCOVERY_RESP) {
             bool already = false;
-            for (int i = 0; i < count; i++)
-            {
-                if (strcmp(found_ips[i], resp.server_ip) == 0)
-                {
+            for (int i = 0; i < count; i++) {
+                if (strcmp(found_ips[i], resp.server_ip) == 0) {
                     already = true;
                     break;
                 }
             }
-            if (!already)
-            {
+            if (!already) {
                 strncpy(found_ips[count], resp.server_ip, 63);
                 found_ips[count][63] = '\0';
                 count++;
@@ -168,29 +143,25 @@ int discover_servers(int sock, char found_ips[][64], int max_servers)
 }
 
 // Envoyer ses données
-void send_data(int sock, PlayerPacket packet, const char *target_ip)
-{
+void send_data(int sock, PlayerPacket packet, const char* target_ip) {
     struct sockaddr_in target;
     target.sin_family = AF_INET;
     target.sin_port = htons(PORT);
     target.sin_addr.s_addr = inet_addr(target_ip);
 
-    sendto(sock, (char *)&packet, sizeof(PlayerPacket), 0, (struct sockaddr *)&target, sizeof(target));
+    sendto(sock, (char*)&packet, sizeof(PlayerPacket), 0, (struct sockaddr*)&target, sizeof(target));
 }
 
 // Recevoir les données d'un autre joueur (Non-bloquant)
-bool receive_data(int sock, PlayerPacket *packet, struct sockaddr_in *from_addr)
-{
+bool receive_data(int sock, PlayerPacket* packet, struct sockaddr_in* from_addr) {
     int from_len = sizeof(struct sockaddr_in);
-    int res = recvfrom(sock, (char *)packet, sizeof(PlayerPacket), 0, (struct sockaddr *)from_addr, &from_len);
-    if (res > 0)
-    {
+    int res = recvfrom(sock, (char*)packet, sizeof(PlayerPacket), 0, (struct sockaddr*)from_addr, &from_len);
+    if (res > 0) {
         return true;
     }
     return false;
 }
 
-void clean_networking()
-{
+void clean_networking() {
     WSACleanup();
 }
